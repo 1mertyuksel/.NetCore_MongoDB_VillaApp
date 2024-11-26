7de3e07e0cb5f652b28010b2d4d81000dc2b462d
1mertyuksel <mertyuksel680@gmail.com>
2024-11-26 18:35:31 +0300

    Add project files.

---
+++ Villa.DataAccess/Context/VillaContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Entity.Entities;
+
+namespace Villa.DataAccess.Context
+{
+    public class VillaContext : DbContext
+    {
+        public VillaContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Counter> Counters { get; set; }
+        public DbSet<Deal> Deal { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Quest> Quests { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Banner>().ToCollection("Banners");
+            modelBuilder.Entity<Contact>().ToCollection("Banners");
+            modelBuilder.Entity<Counter>().ToCollection("Banners");
+            modelBuilder.Entity<Deal>().ToCollection("Banners");
+            modelBuilder.Entity<Feature>().ToCollection("Banners");
+            modelBuilder.Entity<Message>().ToCollection("Banners");
+            modelBuilder.Entity<Product>().ToCollection("Banners");
+            modelBuilder.Entity<Quest>().ToCollection("Banners");
+            modelBuilder.Entity<Video>().ToCollection("Banners");
+        }
+    }
+}
