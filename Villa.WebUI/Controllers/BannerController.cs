76fb5a6a613d761ab14a7381425928714fc66fc7
1mertyuksel <mertyuksel680@gmail.com>
2024-11-28 18:33:44 +0300

    Added CreaterBanner.

--- Villa.WebUI/Controllers/BannerController.cs
+++ Villa.WebUI/Controllers/BannerController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using Villa.Business.Abstract;
 using Villa.Dto.Dtos.BannerDtos;
 using Villa.Entity.Entities;
@@ -23,6 +24,26 @@ namespace Villa.WebUI.Controllers
             var values = await _bannerService.TGetListAsync();
             var bannerList = _mapper.Map<List<ResultBannerDto>>(values);
             return View(bannerList);
+        }
+        public async Task<IActionResult> DeleteBanner(ObjectId id)
+        {
+            await _bannerService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var newBanner = _mapper.Map<Banner>(createBannerDto);
+
+            await _bannerService.TCreateAsync(newBanner);
+            return RedirectToAction("Index");
+
         }
     }
 }
